8734ed9fff233a51123149ed20ed92eb264ae3d8
Bijoya Brakenhoff <>
2019-06-14 18:12:27 +0200

    Use bootstrap

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   'extends': 'stylelint-config-recommended',
+  "plugins": ["stylelint-scss"],
   'rules': {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
     'indentation': 2,
     'string-quotes': 'single',
     'no-duplicate-selectors': true,
