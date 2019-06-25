df63195a087415f5dc5e211526978b5a083cca31
Bijoya Brakenhoff <>
2019-06-25 21:08:11 +0200

    Start building radio buttons for selecting icon set

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  iconSetCardIsOpen = false;
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {
+  constructor(private _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group({
+      iconSet: ['', Validators.required]
+    });
+
+    this.form.valueChanges.subscribe((value) => {
+      console.log(`%cBijoya: settings.component -> valueChanges`, 'color: deeppink;', value);
+      this.iconSetCardIsOpen = false;
+    });
   }
 
+  ngOnInit() {
+
+  }
+
+  openIconSetCard() {
+    this.iconSetCardIsOpen = true;
+  }
 }
