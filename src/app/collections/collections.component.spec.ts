8f4c2adf0c7d9187d9833cb1497bcf6225d3b6d2
Bijoya Brakenhoff <bijoya.brakenhoff@politie.nl>
2019-07-12 08:18:48 +0200

    Fix unit test collections component

--- src/app/collections/collections.component.spec.ts
+++ src/app/collections/collections.component.spec.ts
@@ -1,16 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CollectionsComponent } from './collections.component';
+import { StorageService } from '../core/storage.service';
+import { mock, instance, when } from 'ts-mockito';
 
 describe('CollectionsComponent', () => {
   let component: CollectionsComponent;
   let fixture: ComponentFixture<CollectionsComponent>;
 
+  const storageServiceMock = mock(StorageService);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CollectionsComponent ]
-    })
-    .compileComponents();
+      declarations: [CollectionsComponent],
+      providers: [
+        {
+          provide: StorageService,
+          useFactory: () => {
+            when(storageServiceMock.collections).thenReturn([]);
+            return instance(storageServiceMock);
+          }
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
